2ae6611cc240606427a1188b5b4c1f6839405099
FHirondelle <fleur.dhirondelle@gmail.com>
2020-11-23 18:09:33 +0100

    ajout de tests file 4

--- tests/test_files.py
+++ tests/test_files.py
@@ -11,3 +11,27 @@ def test_defiler():
 
 def test_str():
     assert False
+
+@pytest.fixture()
+def file_quatre():
+    f = File4(5)
+    f.enfiler(1)
+    f.enfiler(2)
+    f.enfiler(3)
+    return f
+
+file_quatre_bis = file_quatre
+
+@pytest.mark.parametrize("origine, attendu", [(file_quatre, "1 -> 2 -> 3"),
+                                              (file_quatre.enfiler(4).depiler(), "2 -> 3 -> 4")])
+def test_file_quatre(origine, attendu):
+    assert str(origine) == attendu
+
+def test_file_quatre_bis():
+    assert not file_quatre_bis.est_vide()
+    assert file_quatre_bis.defiler == 1
+    file_quatre_bis.enfiler(4)
+    file_quatre_bis.enfiler(5)
+    file_quatre_bis.enfiler(6)
+    assert len(file_quatre_bis) == 5
+    assert file_quatre_bis.defiler == 2
