1b73f6ee9fffe5e3d11a1751dbd5ed157dbefe47
FHirondelle <fleur.dhirondelle@gmail.com>
2020-11-27 09:38:46 +0100

    implémentation du tri à bulles

--- tests/test_tris.py
+++ tests/test_tris.py
@@ -3,5 +3,16 @@ from algo.tris import tri_selection, tri_insertion, tri_bulle, tri_fusion
 def test_tris():
     # doit tester que les différents algorithmes de tris tries effectivement
     # les tableaux qu'on leur soumet
-    assert False # à remplacer par les tests effectifs 
+    tableau = [7, 8, 4, 0, -3]
+    tableau_bis = [9, 8, 7, 6]
+    tableau_ter = [1, 2, 3, 4, 5]
+    tableau_vide = []
+    tri_bulle(tableau)
+    tri_bulle(tableau_bis)
+    tri_bulle(tableau_ter)
+    tri_bulle(tableau_vide)
+    assert tableau == [-3, 0, 4, 7, 8]
+    assert tableau_bis == [6, 7, 8, 9]
+    assert tableau_ter == [1, 2, 3, 4, 5]
+    assert tableau_vide == []
 
