f6dc7de385b8d8643fb6d26dbb922cb99a4e576b
Romain9 <romain.msiah@gmail.com>
2020-11-27 19:57:22 +0100

    pile2, test_piles2, tris selection et test_tris

--- algo/tris.py
+++ algo/tris.py
@@ -10,21 +10,10 @@ def tri_selection(tab):
     return tab
 
 def tri_insertions(tab):
-    for i in range(1, len(tab)):
-        j = i
-        k = tab[i]
-        while j > 0 and tab[j - 1] > k:
-            tab[j] = tab[j - 1]
-            j = j - 1
-        tab[j] = k
-    return tab
+    pass
 
 def tri_bulle(tab):
-    for i in range(0, len(tab) - 1):
-        for j in range(len(tab) - i - 1):
-            if tab[j] > tab[j + 1]:
-                tab[j], tab[j + 1] = tab[j + 1], tab[j]
-    return tab
+    pass
 
 def tri_fusion(tab):
     pass
