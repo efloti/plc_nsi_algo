a7a8ecbee49249276a43e2fcc64752a6ddf33228
Romain9 <romain.msiah@gmail.com>
2020-11-27 09:29:01 +0100

    pile2, test_piles2, tri selection, insertion, bulle (je n'ai pas réussi le tri fusion) et test_tris

--- algo/tris.py
+++ algo/tris.py
@@ -1,13 +1,30 @@
 """Implémentation de différents algorithmes de tris."""
 
 def tri_selection(tab):
-    pass
+    for i in range(len(tab) - 1):
+        k = i
+        for j in range(i + 1, len(tab)):
+            if tab[j] < tab[k]:
+                k = j
+        tab[i], tab[k] = tab[k], tab[i]
+    return tab
 
 def tri_insertion(tab):
-    pass
+    for i in range(1, len(tab)):
+        j = i
+        k = tab[i]
+        while j > 0 and tab[j - 1] > k:
+            tab[j] = tab[j - 1]
+            j = j - 1
+        tab[j] = k
+    return tab
 
 def tri_bulle(tab):
-    pass
+    for i in range(0, len(tab) - 1):
+        for j in range(len(tab) - i - 1):
+            if tab[j] > tab[j + 1]:
+                tab[j], tab[j + 1] = tab[j + 1], tab[j]
+    return tab
 
 def tri_fusion(tab):
     pass
--- tests/test_tris.py
+++ tests/test_tris.py
@@ -1,7 +1,19 @@
 from algo.tris import tri_selection, tri_insertion, tri_bulle, tri_fusion
 
 def test_tris():
-    # doit tester que les différents algorithmes de tris tries effectivement
-    # les tableaux qu'on leur soumet
-    assert False # à remplacer par les tests effectifs 
+    tab1 = [1,7,6,8,1,3,4]
+    tab2 = [10,80,7,5,94,27,54]
 
+    res1 = tri_selection(tab1)
+    res2 = tri_selection(tab2)
+    res3 = tri_insertion(tab1)
+    res4 = tri_insertion(tab2)
+    res5 = tri_bulle(tab1)
+    res6 = tri_bulle(tab2)
+
+    assert res1 == [1, 1, 3, 4, 6, 7, 8]
+    assert res2 == [5, 7, 10, 27, 54, 80, 94]
+    assert res3 == [1, 1, 3, 4, 6, 7, 8]
+    assert res4 == [5, 7, 10, 27, 54, 80, 94]
+    assert res5 == [1, 1, 3, 4, 6, 7, 8]
+    assert res6 == [5, 7, 10, 27, 54, 80, 94]
