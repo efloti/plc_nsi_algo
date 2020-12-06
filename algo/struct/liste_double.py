332b0abd28f837d5c41453ba4088c62b89934a83
Romain9 <romain.msiah@gmail.com>
2020-12-06 16:19:25 +0100

    Ajout de __init__, tete, queue, __len__, __str__, _inserer de la classe ListeDbl.

--- algo/struct/liste_double.py
+++ algo/struct/liste_double.py
@@ -12,44 +12,61 @@ rapidement...
 
 
 class Cellule:
-    pass
+    def __init__(self, valeur):
+        self.valeur = valeur
+        self._precedente = None
+        self._suivante = None
 
 
 class ListeDbl:
 
     def __init__(self):
-        pass
+        self._tete = None
+        self._queue = None
+        self._taille = 0
 
     @property
     def tete(self):
-        pass
+        return self._tete
 
     @property
     def queue(self):
-        pass
+        return self._queue
 
     def __len__(self):
-        pass
+        return self._taille
 
     def __str__(self):
-        pass
+        val = self._tete
+        afficher = []
+        while val:
+            afficher.append(val.valeur)
+            val = val._suivante
+        return str(afficher)
 
     def _inserer(self, valeur, cellule=None):
         """insere valeur juste après la cellule précisée
         Si elle n'est pas précisé, l'insertion a lieue en tête."""
-        pass
+        if tete is True:
+            self._tete = Cellule(valeur)
+        else:
+            self._queue = Cellule(valeur)
+
 
     def _supprimer(self, cellule):
         """Supprime la cellule indiquée et renvoie sa valeur"""
-        pass
+
+
 
     def inserer(self, valeur, tete=True):
         """insertion en tete ou en queue si l'argument optionnel vaut False"""
-        pass
+
+
+
 
     def supprimer(self, valeur, tete=True):
         """suppression en tete ou en queue si l'argument optionnel vaut False
         Renvoie la valeur de la cellule supprimée."""
-        pass
+
 
     # on peut ajouter - bonus - d'autres méthodes utiles (voir liste simple)
